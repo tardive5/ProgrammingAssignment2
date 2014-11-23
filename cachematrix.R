f546a12ed7fb400c2143fabe593361291625afbf
So Tardivel <so.tardivel@free.fr>
2014-11-23 16:24:07 +0100

    my work

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,74 @@
-## Put comments here that give an overall description of what your
-## functions do
+#####################################################################################################################################
+## makeCacheMatrix and cacheSolve are designed to avoid the re-computation of inverted Matrix.
+## A special object is created to keep the inverted matrix into a memory cache, associated to the matrix
+## cacheSolve checks if the cache is virgin before computing the inverted matrix. if not, it only returns existing cache content.
+#####################################################################################################################################
 
-## Write a short comment describing this function
+## MakeCacheMatrix creates a special matrix object
+## PARAMETERS
+## x : mandatory input parameter, Matrix type, the original matrix
+##
+## LOCAL VARIABLES 
+## lInv : Inverted Matrix; kept in memory cache within the special object
+##
+## FUNCTIONS : accessors and mutators for matrix and its inverted matrix
+##
+## RETURN a list of functions, set, get, getInv, setInv
 
 makeCacheMatrix <- function(x = matrix()) {
-
+                
+        ## INITIALIZATION of Inverted Matrix at object creation
+        lInv <- NULL
+        
+        ## SET/GET FUNCTIONS for matrix
+        set <- function(pNM) {
+                ## PARAMETER pNM: New Matrix 
+                x <<- pNM
+                lInv <<- NULL
+        }
+        
+        get <- function() x
+        
+        ## SET/GET FUNCTIONS for Inverted matrix
+        
+        setInv <- function(pInv) lInv <<- pInv
+        
+        getInv <- function () lInv
+        
+        ## RETURN the list of function references
+        list(set = set, get = get,
+             setInv = setInv,
+             getInv = getInv)
 }
 
 
-## Write a short comment describing this function
+## cacheSolve computes the inverted Matrix
+## PARAMETERS
+## x : mandatory input parameter; Supposed to be special Matrix Object type
+##
+## LOCAL VARIABLES 
+## lInv : Reference to inverted matrix of x
+##
+## RETURN lInv, computed here or already cached in the special Matrix Object
+
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+        
+        lInv <- x$getInv()
+        
+        if(!is.null(lInv)) {
+                ## Already computed.
+                message("getting cached Inverted Matrix")
+        }
+        else
+        { 
+                ## COMPUTE the inverted matrix of x 
+                lM <- x$get()
+                lInv <- solve(lM, ...)
+                ## Cache the result for next computation
+                x$setInv(lInv)
+        }
+        
+        ## RETURN inverted matrix reference
+        lInv
 }
